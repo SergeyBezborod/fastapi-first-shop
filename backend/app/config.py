54ab3eaecaa840b7568ffdb0f073c8e458817c36
Sergey Bezborodov <i41nana@yandex.ru>
2025-11-18 15:00:45 +0700

    Initial commit

---
+++ backend/app/config.py
@@ -0,0 +1,26 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    # Название приложения
+    app_name: str = "FastAPI Shop"
+    # Режим отладки - вкл
+    debug: bool = True
+    # Путь до базы
+    database_url: str = "sqlite:///./shop.db"
+    # Пути, с которых будут приниматься запросы от фронтенда
+    cors_origins: list = [
+        "http://localhost:5173",
+        "http://localhost:3000",
+        "http://127.0.0.1:5173",
+        "http://127.0.0.1:3000",
+    ]
+    static_dir: str = "static"
+    image_dir: str = "static/images"
+
+    class Config:
+        env_file = ".env"
+
+
+# Инициализируем настройки в переменную
+settings = Settings()
---
+++ backend/app/database.py
@@ -0,0 +1,30 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from .config import settings
+
+engine = create_engine(
+    settings.database_url,
+    connect_args={
+        "check_same_thread": False
+    }
+)
+
+# Создатель сессий
+SessionLocal = sessionmaker(autocommit = False, autoflush=False, bind=engine)
+# Базовый класс для моделей
+Base = declarative_base()
+
+
+# Каждый раз, когда нужна БД, открывается сессия
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+# Инициалазация БД 
+def init_db():
+    Base.metadata.create_all(bind=engine)
